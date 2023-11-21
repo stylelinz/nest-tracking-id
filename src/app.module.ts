4b8c4419cc199f93a8d457fab02d9d10842a22c6
stylelinz_mac <cwchuang1997@gmail.com>
2023-11-21 21:22:37 +0800

    feat: add tracking id & dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClsModule } from 'nestjs-cls';
+import { randomUUID } from 'crypto';
+import { Response } from 'express';
 
 @Module({
-  imports: [],
+  imports: [
+    ClsModule.forRoot({
+      global: true,
+      interceptor: {
+        mount: true,
+        setup(cls, context) {
+          const res = context.switchToHttp().getResponse<Response>();
+          const uuid = randomUUID();
+          cls.set('user-id', uuid);
+          res.setHeader('x-tracking-id', uuid);
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
